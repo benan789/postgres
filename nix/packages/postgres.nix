20e5a76dd96d4d1c79d465b36ac3d366a6c49139
Ben <benan789@gmail.com>
2026-06-26 11:17:07 +0800

    undos

--- nix/packages/postgres.nix
+++ nix/packages/postgres.nix
@@ -39,6 +39,7 @@
         ../ext/pg_net.nix
         ../ext/pg_hashids.nix
         ../ext/pgsodium.nix
+        ../ext/pg_graphql
         ../ext/pg_stat_monitor.nix
         ../ext/pg_jsonschema
         ../ext/pg_partman.nix
